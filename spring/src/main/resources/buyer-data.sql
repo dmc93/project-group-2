ee4ca26e0ce788325795be5031af952457aee119
sparku7 <sparku7@sky.com>
2024-07-05 13:10:41 +0100

    Added further buyer testing.

--- spring/src/main/resources/buyer-data.sql
+++ spring/src/main/resources/buyer-data.sql
@@ -1,1 +1,6 @@
-INSERT INTO buyer (firstname, surname) VALUES ('John', 'Doe');
+INSERT INTO buyer (firstname, surname)
+VALUES
+    ('John', 'Doe'),
+    ('Jane', 'Doe'),
+    ('Keir', 'Starmer');
+
--- spring/src/test/java/com/wrongmove/demo/tests/BuyerControllerTest.java
+++ spring/src/test/java/com/wrongmove/demo/tests/BuyerControllerTest.java
@@ -2,6 +2,9 @@ package com.wrongmove.demo.tests;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wrongmove.demo.entities.Buyer;
+import net.bytebuddy.dynamic.DynamicType;
+import org.apache.coyote.Request;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -23,26 +26,79 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class BuyerControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private MockMvc mvc;
 
     @Autowired
-    private ObjectMapper objectMapper;
+    private ObjectMapper mapper;
 
     @Test
     void testCreateBuyer() throws Exception {
 
         Buyer newBuyer = new Buyer("Jane", "Doe");
-        String newBuyerAsJson = objectMapper.writeValueAsString(newBuyer);
+        String newBuyerAsJson = this.mapper.writeValueAsString(newBuyer);
+
+        RequestBuilder mockRequest = MockMvcRequestBuilders.post("/buyer/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(newBuyerAsJson);
 
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/buyer/add")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(newBuyerAsJson))
-                .andExpect(MockMvcResultMatchers.status().isCreated())
+        ResultMatcher checkStatus = MockMvcResultMatchers.status().isCreated();
+
+        mvc.perform(mockRequest)
+                .andExpect(checkStatus)
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.firstname").value("Jane"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.surname").value("Doe"));
+    }
+
+    @Test
+    void testDeleteBuyer() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders.delete("/buyer/remove/3"))
+
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Buyer with id 3 has been deleted."));
+    }
+
+    @Test
+    void testDeleteBuyerDoesNotExist() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders.delete("/buyer/remove/4"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.content().string("No buyer found with id 4"));
+    }
+
+    @Test
+    void testGetAllBuyers() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/buyer/get/all")
+
+                        .accept(MediaType.APPLICATION_JSON))
+
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$").isArray())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(3)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].firstname").value("John"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].surname").value("Doe"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].firstname").value("Jane"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].surname").value("Doe"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[2].firstname").value("Keir"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[2].surname").value("Starmer"));
+    }
+
+    @Test
+    void testGetBuyerById() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/buyer/get/2")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(2))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.firstname").value("Jane"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.surname").value("Doe"));
     }
 
-
+    @Test
+    void testGetBuyerByIdBuyerDoesNotExist() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/buyer/get/4"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andExpect(MockMvcResultMatchers.content().string("No buyer found with id 4"));
+    }
 }
